64cf8f910efdf619b481286b7c41f2d22a5e73f6
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-01-29 14:25:32 -0500

    created react-rainbow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
+import './App.css'
 import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
 
 
 function App(){
@@ -18,11 +21,16 @@ function App(){
         )
     })
 
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+      }
+    
     return (
       <div className="App">
       {colors.map((color, i) => 
           <ColorBlock key={i} color={color} />
       )}
+      <ColorForm/>
   </div>
     )
 }
